e791bf93ffda52bfca2759262275907a83e890f1
Sumin Park <smpark@se.kaist.ac.kr>
2019-07-13 16:11:14 +0900

    Remove unnecessary excel files. Terminate GUI after ending condition. (300 frame, 2 simulation) Write saved rate. (After the treatment from the hospital) Treatment takes 10 frame

--- src/engine/src/misc/ExcelHelper.java
+++ src/engine/src/misc/ExcelHelper.java
@@ -1,6 +1,5 @@
 package misc;
 
-import com.sun.corba.se.spi.orbutil.threadpool.Work;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
