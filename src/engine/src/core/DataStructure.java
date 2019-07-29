05e8f87f3b6cd1fc6d3e859476a13d9a1dcc1779
sumin0407 <smpark@se.kaist.ac.kr>
2019-07-29 19:22:12 +0900

    Create DataStructure.java
    public static final ArrayList<DataStructure.AddCS> addCS = new ArrayList<>();
    public static final ArrayList<DataStructure.Message> message = new ArrayList<>();
    public static final ArrayList<DataStructure.Range> range = new ArrayList<>();
    public static final ArrayList<DataStructure.ChangeAll> changeAll = new ArrayList<>();
    public static final ArrayList<DataStructure.ChangeOne> changeOne = new ArrayList<>();
    public static final ArrayList<DataStructure.RemoveCS> removeCS = new ArrayList<>();

---
+++ src/engine/src/core/DataStructure.java
@@ -0,0 +1,94 @@
+package core;
+
+public class DataStructure {
+
+    public static class AddCS {
+        public String command;
+        public int frame;
+        public int count;
+
+        public AddCS(String command, int frame, int count) {
+            this.command = command;
+            this.frame = frame;
+            this.count = count;
+        }
+    }
+
+    // Structure o message stimuli
+    public static class Message {
+        public String command;
+        public int startFrame;
+        public int finishFrame;
+        public String sender;
+        public String receiver;
+        public int duration;
+
+        public Message(String command, int startFrame, int finishFrame, String sender, String receiver, int duration) {
+            this.command = command;
+            this.startFrame = startFrame;
+            this.finishFrame = finishFrame;
+            this.sender = sender;
+            this.receiver = receiver;
+            this.duration = duration;
+        }
+    }
+
+    public static class Range {
+        public String command;
+        public int frame;
+        public int left;
+        public int top;
+        public int right;
+        public int bottom;
+        public Object value;
+
+        public Range(String command, int frame, int left, int top, int right, int bottom, Object value) {
+            this.command = command;
+            this. frame = frame;
+            this.left = left;
+            this.top = top;
+            this.right = right;
+            this.bottom = bottom;
+            this.value = value;
+        }
+    }
+
+    public static class ChangeAll {
+        public String command;
+        public int frame;
+        public Object value;
+
+        public ChangeAll(String command, int frame, Object value) {
+            this.command = command;
+            this.frame = frame;
+            this.value = value;
+        }
+    }
+
+    public static class ChangeOne {
+        public String command;
+        public int frame;
+        public int number;
+        public Object value;
+
+        public ChangeOne(String command, int frame, int number, Object value) {
+            this.command = command;
+            this.frame = frame;
+            this.number = number;
+            this.value = value;
+        }
+    }
+
+    public static class RemoveCS {
+        public String command;
+        public int frame;
+        public int number;
+
+        public RemoveCS(String command, int frame, int number) {
+            this.command = command;
+            this.frame = frame;
+            this.number = number;
+        }
+    }
+
+}
