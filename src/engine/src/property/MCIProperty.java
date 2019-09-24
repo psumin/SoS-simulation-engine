1c99d32aba7ad0cebf134abaf8382c45accefaca
abalon1210 <abalon1210@daum.net>
2019-09-24 16:45:14 +0900

    SoS General Property with Existence

---
+++ src/engine/src/property/MCIProperty.java
@@ -0,0 +1,21 @@
+package property;
+
+public class MCIProperty extends Property {
+
+  double rescueRate;
+  double thresholdPatient;
+  public MCIProperty(String name, String specification, String propertyType, double etc) {
+    super(name, specification, propertyType);
+    this.rescueRate = etc;
+  }
+
+  public void setThresholdPatient(double thresholdPatient) {
+    this.thresholdPatient = thresholdPatient;
+  }
+  
+  public double getValue() {
+    return this.rescueRate;
+  }
+  
+  public double getThresholdPatient() {return this.thresholdPatient;}
+}
