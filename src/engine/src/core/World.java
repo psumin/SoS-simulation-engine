b1c51f6e1604ac1d28752da282f3e47bfb65c1f1
sumin0407 <smpark@se.kaist.ac.kr>
2019-11-07 12:39:59 +0900

    Nearest distance to farthest distance  with scenario.

--- src/engine/src/core/World.java
+++ src/engine/src/core/World.java
@@ -375,7 +375,7 @@ public class World extends SoSObject {
 //            if(i == 0){
 //                hospital.setCapacity(2);
 //            } else {
-            hospital.setCapacity(2);
+            hospital.setCapacity(10);
 //            }
         }
         hospitals.get(0).setPosition(0, 0);
