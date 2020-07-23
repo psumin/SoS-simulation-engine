cc3f1f3e8c02aaa5432b5cedb914b4f40e001459
sumin0407 <smpark@se.kaist.ac.kr>
2020-07-23 14:19:23 +0900

    implement the additional checkers at main file. (now annotated!)

--- src/engine/src/main.java
+++ src/engine/src/main.java
@@ -13,13 +13,14 @@ public class main {
         long thetaStartTime;                                            // 한 사이클 시작 시간
         long thetaEndTime;                                              // 한 사이클 종료 시간
         boolean ruuning = true;
+
+/*
         //verification
-//        SPRT verifier;
+        SPRT verifier;
         //ComfortZoneChecker comfortZoneChecker = new ComfortZoneChecker();
-        
+
         // Existence
-//        MCIProperty property = new MCIProperty("RescuePatientProperty", "RescuedPatientRatioUpperThanValue", "MCIExistence", 0.1);
-//        MCIProperty property = new MCIProperty("TreatmentPatientProperty", "TreatmentPatientRatioUpperThanValue", "MCIExistence", 0.5);
+        MCIProperty property = new MCIProperty("RescuePatientProperty", "RescuedPatientRatioUpperThanValue", "MCIExistence", 0.02);
         // Absence
 //        property.setThresholdValue(0); // RescueRate - TreatmentRate
         // Universality
@@ -35,24 +36,35 @@ public class main {
 //        property.setThresholdValue(0); // Rescurate
 //        property.setDuration(60); // 최대 60 Frame 이하
         // Bounded Existence
-//        property.setDuration(33); // Bounded Frame 40
+//        property.setDuration(15); // Bounded Frame 20
 //        property.setState("Free"); // State가 Free인게 아님을 확인하기 위해
-//        // Precedence
+        // Precedence
 //        property.setPrevState("MoveToPatient");
 //        property.setState("FirstAid");
-        
-//        MCIPropertyChecker existenceChecker = new MCIPropertyChecker();
-//        MCIAbsenceChecker absenceChecker = new MCIAbsenceChecker();
-//        MCIUniversalityChecker universalityChecker = new MCIUniversalityChecker();
-//        MCITransientSPChecker transientSPChecker = new MCITransientSPChecker();
-//        MCISteadySPChecker steadySPChecker = new MCISteadySPChecker();
-//        MCIMinimumDurationChecker minimumDurationChecker = new MCIMinimumDurationChecker();
-//        MCIMaximumDurationChecker maximumDurationChecker = new MCIMaximumDurationChecker();
-//        MCIBoundedExistenceChecker boundedExistenceChecker = new MCIBoundedExistenceChecker();
-//        MCIPrecedenceChecker precedenceChecker = new MCIPrecedenceChecker();
-        
+        // Response
+//        property.setPrevState("FirstAid");
+//        property.setState("TransferToBridgehead");
+        // Recurrence
+//        property.setPrevState("MoveToPatient");
+//        property.setThresholdValue(51);
+        // Until
+        property.setPrevState("Free");
+
+        MCIPropertyChecker existenceChecker = new MCIPropertyChecker();
+        MCIAbsenceChecker absenceChecker = new MCIAbsenceChecker();
+        MCIUniversalityChecker universalityChecker = new MCIUniversalityChecker();
+        MCITransientSPChecker transientSPChecker = new MCITransientSPChecker();
+        MCISteadySPChecker steadySPChecker = new MCISteadySPChecker();
+        MCIMinimumDurationChecker minimumDurationChecker = new MCIMinimumDurationChecker();
+        MCIMaximumDurationChecker maximumDurationChecker = new MCIMaximumDurationChecker();
+        MCIBoundedExistenceChecker boundedExistenceChecker = new MCIBoundedExistenceChecker();
+        MCIPrecedenceChecker precedenceChecker = new MCIPrecedenceChecker();
+        MCIResponseChecker responseChecker = new MCIResponseChecker();
+        MCIRecurrenceChecker recurrenceChecker = new MCIRecurrenceChecker();
+        MCIUntilChecker untilChecker = new MCIUntilChecker();
+
 //        verifier = new SPRT(comfortZoneChecker);
-        
+
 //        verifier = new SPRT(existenceChecker);
 //        verifier = new SPRT(absenceChecker);
 //        verifier = new SPRT(universalityChecker);
@@ -62,7 +74,12 @@ public class main {
 //        verifier = new SPRT(maximumDurationChecker);
 //        verifier = new SPRT(boundedExistenceChecker);
 //        verifier = new SPRT(precedenceChecker);
-//        Pair<Pair<Integer, Boolean>, String> verificationResult;
+//        verifier = new SPRT(responseChecker);
+//        verifier = new SPRT(recurrenceChecker);
+        verifier = new SPRT(untilChecker);
+        Pair<Pair<Integer, Boolean>, String> verificationResult;
+
+*/
 
         SoSSimulationProgram simulationEngine = new SoSSimulationProgram();
         simulationEngine.setRunning();
@@ -71,38 +88,38 @@ public class main {
         simulationEngine.run();
         simulationEngine.setSuper_counter();
 
-//        double satisfactionProb = 0;
-//        Boolean satisfaction = true;
+        double satisfactionProb = 0;
+        Boolean satisfaction = true;
 
-//        for (int i = 1; i < 100; i++) {
-////            programStartTime = System.nanoTime();           // 첫번째 시뮬레이션을 제외하려면 여기에 정의
-//
-////            System.out.println("inside for loop:" + simulationEngine.getRunning());
-//            double theta = i * 0.01;
-//
-//            thetaStartTime = System.nanoTime();
-//            verificationResult = verifier.verifyWithSimulationGUI(simulationEngine, property, 400, theta);    //or T = 3
-//            thetaEndTime = System.nanoTime();
-//            System.out.println(i /(float)100 + " theta verification running time: " + (thetaEndTime - thetaStartTime) / (float)1000_000_000 + " sec");          // 한 theta 실행 시간
-//
-//            System.out.println(verificationResult.getValue());
-//            if (satisfaction == true && !verificationResult.getKey().getValue()) {
-//                satisfactionProb = theta;
-//                satisfaction = false;
-//            }
-//
-//            if(i == 1 && satisfaction == false) {
-//                satisfactionProb = 0.0;
-//            }
-//
-//        }
-//        if (satisfaction == true) {
-//            satisfactionProb = 1;
-//        }
-//        System.out.println("Verification property satisfaction probability: " + satisfactionProb);
+/*
+
+        for (int i = 1; i < 100; i++) {
+//            programStartTime = System.nanoTime();           // 첫번째 시뮬레이션을 제외하려면 여기에 정의
+
+//            System.out.println("inside for loop:" + simulationEngine.getRunning());
+            double theta = i * 0.01;
+
+            thetaStartTime = System.nanoTime();
+            verificationResult = verifier.verifyWithSimulationGUI(simulationEngine, property, 2000, theta);    //or T = 3
+            thetaEndTime = System.nanoTime();
+            System.out.println(i /(float)100 + " theta verification running time: " + (thetaEndTime - thetaStartTime) / (float)1000_000_000 + " sec");          // 한 theta 실행 시간
+
+            System.out.println(verificationResult.getValue());
+            if (satisfaction == true && !verificationResult.getKey().getValue()) {
+                satisfactionProb = theta;
+                satisfaction = false;
+            }
+
+        }
+        if (satisfaction == true) {
+            satisfactionProb = 1;
+        }
+        System.out.println("Verification property satisfaction probability: " + satisfactionProb);
         programEndTime = System.nanoTime();
         System.out.println("=== Total Program running time: " + (programEndTime - programStartTime) / (float)1000_000_000 + " sec");          // 전체 프로그램 실행 시간
 
 //        new Thread(simulationEngine).start();
+
+*/
     }
 }
